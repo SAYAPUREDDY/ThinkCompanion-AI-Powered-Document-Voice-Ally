776f9b5445a40ecd5614209ed2b831b1c9d4e1fb
Sayapureddy <tejasurya0953@gmail.com>
2025-01-07 12:08:36 +0100

    updated route mapping

--- routes/routes_mapping.py
+++ routes/routes_mapping.py
@@ -3,6 +3,7 @@ from routes.upload_routes import router as upload_router
 from routes.chat_routes import router as chat_router
 from routes.session_routes import router as session_router
 from routes.history_routes import router as history_router
+from routes.user import router as user_router
 
 def include_routes(app: FastAPI):
     """
@@ -12,3 +13,6 @@ def include_routes(app: FastAPI):
     app.include_router(chat_router, prefix="/chat", tags=["Chat"])
     app.include_router(session_router, prefix="/sessions", tags=["Sessions"])
     app.include_router(history_router, prefix="/history", tags=["History"])
+    app.include_router(user_router, prefix="/user", tags=["User"])
+
+
